cf48c8a641eb24163c91c653bf7ed761d8a4bcb8
Karl Duderstadt <duderstadt@biochem.mpg.de>
2019-09-11 16:55:34 +0200

    update insets for table window tabs

--- src/main/java/de/mpg/biochem/mars/fx/table/MarsTableFxFrame.java
+++ src/main/java/de/mpg/biochem/mars/fx/table/MarsTableFxFrame.java
@@ -145,7 +145,7 @@ public class MarsTableFxFrame implements MarsTableWindow {
 		
 		tabPane.setStyle("");
 		tabPane.getStylesheets().clear();
-		tabPane.getStylesheets().add("de/mpg/biochem/mars/fx/molecule/moleculesTab/MoleculeTablesPane.css");
+		tabPane.getStylesheets().add("de/mpg/biochem/mars/fx/table/TableWindowPane.css");
 		
 		tabPane.getSelectionModel().select(dataTableTab);
 		
@@ -163,10 +163,6 @@ public class MarsTableFxFrame implements MarsTableWindow {
 	public void close() {
     	
     }
-	
-	public MarsTable getTable() {
-		return table;
-	}
 
 	@Override
 	public void update() {
