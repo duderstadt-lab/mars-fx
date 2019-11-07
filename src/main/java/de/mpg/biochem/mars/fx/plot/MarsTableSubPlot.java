c3553755993f20277d37fefe897f10b75adabcd6
Karl Duderstadt <duderstadt@biochem.mpg.de>
2019-11-07 11:58:58 +0100

    update Table Subplot to add color and width options

--- src/main/java/de/mpg/biochem/mars/fx/plot/MarsTableSubPlot.java
+++ src/main/java/de/mpg/biochem/mars/fx/plot/MarsTableSubPlot.java
@@ -6,6 +6,7 @@ import java.util.List;
 import de.gsi.chart.XYChart;
 import de.gsi.chart.axes.spi.DefaultNumericAxis;
 import de.gsi.dataset.spi.DoubleDataSet;
+import de.mpg.biochem.mars.fx.plot.tools.MarsDoubleDataSet;
 import de.mpg.biochem.mars.fx.plot.tools.SegmentDataSetRenderer;
 import de.mpg.biochem.mars.table.MarsTable;
 import javafx.collections.FXCollections;
@@ -25,120 +26,26 @@ public class MarsTableSubPlot extends AbstractSubPlot {
 	protected void addDataSet(SegmentDataSetRenderer renderer, PlotSeries plotSeries) {
 		String xColumn = plotSeries.getXColumn();
 		String yColumn = plotSeries.getYColumn();
-
-		DoubleDataSet dataset = new DoubleDataSet(plotSeries.getXColumn() + " vs " + plotSeries.getYColumn());
 		
-		List<Double> xValueList = new ArrayList<>();
-		List<Double> yValueList = new ArrayList<>();
+		if (!getDataTable().hasColumn(xColumn) || !getDataTable().hasColumn(yColumn))
+			return;
+		
+		double lineWidth = Double.valueOf(plotSeries.getWidth());
+		
+		MarsDoubleDataSet dataset = new MarsDoubleDataSet(yColumn + " vs " + xColumn, plotSeries.getColor(), lineWidth);
+		
 		for (int row=0;row<getDataTable().getRowCount();row++) {
 			double x = getDataTable().getValue(xColumn, row);
 			double y = getDataTable().getValue(yColumn, row);
 			
 			if (!Double.isNaN(x) && !Double.isNaN(y)) {
-				xValueList.add(x);
-				yValueList.add(y);
+				dataset.add(x, y);
 			}
 		}
-		
-		final double[] xValues = new double[xValueList.size()];
-		final double[] yValues = new double[yValueList.size()];
-		for (int row=0;row<xValueList.size();row++) {
-			xValues[row] = xValueList.get(row);
-			yValues[row] = yValueList.get(row);
-		}
-		
+
 		dataset.setStyle(plotSeries.getType());
-		renderer.getDatasets().add(dataset);
+		renderer.getDatasets().add(dataset);	
 	}
-	
-	/*
-	protected XYChart<Number, Number> addLine(PlotSeries plotSeries) {
-		String xColumn = plotSeries.getXColumn();
-		String yColumn = plotSeries.getYColumn();
-		
-		NumericAxis xAxis = createAxis();
-		NumericAxis yAxis = createAxis();
-		
-		resetXYZoom();
-		
-		resetXAxis(xAxis);
-		resetYAxis(yAxis);
-		
-		LineChartWithSegments lineChart = new LineChartWithSegments(null, plotSeries, xAxis, yAxis);
-		lineChart.setCreateSymbols(false);
-		lineChart.setAnimated(false);
-		
-		List<Data<Number, Number>> data = new ArrayList<>();
-		for (int row=0;row<getDataTable().getRowCount();row++) {
-			double x = getDataTable().getValue(xColumn, row);
-			double y = getDataTable().getValue(yColumn, row);
-			
-			if (!Double.isNaN(x) && !Double.isNaN(y))
-				data.add(new Data<>(x, y));
-		}
-		
-		//If the columns are entirely NaN values. Don't add he plot
-		if (data.size() == 0)
-			return null;
-		
-		ObservableList<Data<Number, Number>> sourceData = FXCollections.observableArrayList(data);
-		
-		DataReducingObservableList<Number, Number> reducedData = new DataReducingObservableList<>(xAxis, sourceData);
-		reducedData.maxPointsCountProperty().bind(plotPane.maxPointsCount());
-		
-		Series<Number, Number> series = new Series<>(plotSeries.getYColumn(), reducedData);
-		lineChart.getData().add(series);
-			
-		lineChart.updateStyle(plotPane.getStyleSheetUpdater());
-		
-		chartPane.getOverlayCharts().add(lineChart);
-		
-		return lineChart;
-	}
-	
-	protected XYChart<Number, Number> addScatter(PlotSeries plotSeries) {
-		String xColumn = plotSeries.getXColumn();
-		String yColumn = plotSeries.getYColumn();
-
-		NumericAxis xAxis = createAxis();
-		NumericAxis yAxis = createAxis();
-		
-		resetXYZoom();
-		
-		resetXAxis(xAxis);
-		resetYAxis(yAxis);
-
-		ScatterChartWithSegments scatterChart = new ScatterChartWithSegments(null, plotSeries, xAxis, yAxis);
-		scatterChart.setAnimated(false);
-		
-		List<Data<Number, Number>> data = new ArrayList<>();
-		for (int row=0;row<getDataTable().getRowCount();row++) {
-			double x = getDataTable().getValue(xColumn, row);
-			double y = getDataTable().getValue(yColumn, row);
-			
-			if (!Double.isNaN(x) && !Double.isNaN(y))
-				data.add(new Data<>(x, y));
-		}
-		
-		//If the columns are entirely NaN values. Don't add he plot
-		if (data.size() == 0)
-			return null;
-		
-		ObservableList<Data<Number, Number>> sourceData = FXCollections.observableArrayList(data);
-		
-		DataReducingObservableList<Number, Number> reducedData = new DataReducingObservableList<>(xAxis, sourceData);
-		reducedData.maxPointsCountProperty().bind(plotPane.maxPointsCount());
-		
-		Series<Number, Number> series = new Series<>(plotSeries.getYColumn(), reducedData);
-		scatterChart.getData().add(series);
-		
-		scatterChart.updateStyle(plotPane.getStyleSheetUpdater());
-		
-		chartPane.getOverlayCharts().add(scatterChart);
-		
-		return scatterChart;
-	}
-*/
 	
 	@Override
 	protected MarsTable getDataTable() {
