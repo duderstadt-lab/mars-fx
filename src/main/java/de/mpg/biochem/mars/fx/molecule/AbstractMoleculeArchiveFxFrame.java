e59cf555cd439bfe9b7042e7a1b504141d32ae0a
Karl Duderstadt <duderstadt@biochem.mpg.de>
2021-11-26 00:22:29 +0100

    Hide Molecule Archive stage in close method if still showing

--- src/main/java/de/mpg/biochem/mars/fx/molecule/AbstractMoleculeArchiveFxFrame.java
+++ src/main/java/de/mpg/biochem/mars/fx/molecule/AbstractMoleculeArchiveFxFrame.java
@@ -1258,6 +1258,9 @@ public abstract class AbstractMoleculeArchiveFxFrame<I extends MarsMetadataTab<?
     
     @Override
     public void close() {
+    	if (stage.isShowing())
+			stage.hide();
+    	
     	if (moleculeArchiveService.contains(archive.getName()))
 			moleculeArchiveService.removeArchive(archive);
 
