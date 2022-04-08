c1bd0607f8720153b50651d046ee70d451bbbda9
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-04-08 15:18:14 +0200

    MarsTableView: fixes performance with dynamic rows generated from table
    Previously all data way copied into the TableView but the performance
    when doing this is horrible. So now the currently table view is
    dynamically generated based only on the currently visible rows without
    any copying required. This is much faster.
    
    Fixes a major performance issue with LUMICKS data

--- src/main/java/de/mpg/biochem/mars/fx/table/MarsTableView.java
+++ src/main/java/de/mpg/biochem/mars/fx/table/MarsTableView.java
@@ -28,14 +28,20 @@
  */
 package de.mpg.biochem.mars.fx.table;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.scijava.table.DoubleColumn;
+
 import de.mpg.biochem.mars.table.MarsTable;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-public class MarsTableView extends TableView<ObservableList<Object>> {
+public class MarsTableView extends TableView<MarsTableView.MarsTableRowObject> {
 	private MarsTable table;
 	
 	public MarsTableView(MarsTable table) {
@@ -45,45 +51,247 @@ public class MarsTableView extends TableView<ObservableList<Object>> {
 	
 	private void buildTableView() {
 		//first add index
-        TableColumn<ObservableList<Object>, Object> rowIndexCol = new TableColumn<>("Index");
+        TableColumn<MarsTableView.MarsTableRowObject, Object> rowIndexCol = new TableColumn<>("Index");
         rowIndexCol.setCellValueFactory(param ->
-                new ReadOnlyObjectWrapper<>(param.getValue().get(0))
+                new ReadOnlyObjectWrapper<>(param.getValue().getRowNumber())
         );
         rowIndexCol.setSortable(false);
         getColumns().add(rowIndexCol);
+        
+        this.setItems(FXCollections.observableList(new MarsTableRowList()));
 
         // add columns
         for (int col = 0; col < table.getColumnCount(); col++) {
-            final int finalIdx = col+1;
-            TableColumn<ObservableList<Object>, Object> column = new TableColumn<>(
-                    table.get(col).getHeader()
-            );
-            column.setCellValueFactory(param ->
-                    new ReadOnlyObjectWrapper<>(param.getValue().get(finalIdx))
-            );
+        	String header = table.get(col).getHeader();
+            //final int finalIdx = col+1;
+            TableColumn<MarsTableView.MarsTableRowObject, Object> column = new TableColumn<>(header);
+            
+            if (table.get(col) instanceof DoubleColumn)
+	            column.setCellValueFactory(row ->
+	                    new ReadOnlyObjectWrapper<>(row.getValue().getValue(header))
+	            );
+            else
+            	column.setCellValueFactory(row ->
+                	new ReadOnlyObjectWrapper<>(row.getValue().getStringValue(header))
+            	);
+            
             column.setSortable(false);
             getColumns().add(column);
         }
-
-        // add data
-        for (int row = 0; row < table.getRowCount(); row++) {
-            getItems().add(
-        		FXCollections.observableArrayList(
-                        table.getRowAsList(row)
-                )
-            );
-        }
 	}
 	
-	public void reloadData() {
-		getItems().clear();
-		// add data
-        for (int row = 0; row < table.getRowCount(); row++) {
-            getItems().add(row,
-        		FXCollections.observableArrayList(
-                        table.getRowAsList(row)
-                )
-            );
-        }
+	class MarsTableRowObject {
+		
+		private final int row;
+		
+		MarsTableRowObject(final int row) {
+			this.row = row;
+		}
+		
+		double getValue(String header) {
+			return table.getValue(header, row);
+		}
+		
+		String getStringValue(String header) {
+			return table.getStringValue(header, row);
+		}
+		
+		int getRowNumber() {
+			return row;
+		}
 	}
+	
+	class MarsTableRowList implements List<MarsTableRowObject> {
+		
+		public MarsTableRowList() {}
+
+		@Override
+		public int size() {
+			return table.getRowCount();
+		}
+
+		@Override
+		public boolean isEmpty() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public Iterator<MarsTableRowObject> iterator() {
+			return null;
+		}
+
+		@Override
+		public Object[] toArray() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public <T> T[] toArray(T[] a) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean add(MarsTableRowObject e) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends MarsTableRowObject> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean addAll(int index, Collection<? extends MarsTableRowObject> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void clear() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public MarsTableRowObject get(int index) {
+			return new MarsTableRowObject(index);
+		}
+
+		@Override
+		public MarsTableRowObject set(int index, MarsTableRowObject element) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void add(int index, MarsTableRowObject element) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public MarsTableRowObject remove(int index) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public int indexOf(Object o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int lastIndexOf(Object o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public ListIterator<MarsTableRowObject> listIterator() {
+			return null;
+		}
+
+		@Override
+		public ListIterator<MarsTableRowObject> listIterator(int index) {
+			return new MarsTableRowObjectListItr(index);
+		}
+
+		@Override
+		public List<MarsTableRowObject> subList(int fromIndex, int toIndex) {
+			return null;
+		}
+	}
+	
+	class MarsTableRowObjectListItr implements ListIterator<MarsTableRowObject> {
+	    private int index;
+
+	    MarsTableRowObjectListItr(int index) {
+	        this.index = index;
+	    }
+
+	    public boolean hasNext() {
+	        return index < table.getRowCount();
+	    }
+
+	    public MarsTableRowObject next() {
+	        MarsTableRowObject obj = new MarsTableRowObject(index);
+	        index++;
+	        return obj;
+	    }
+
+		@Override
+		public boolean hasPrevious() {
+			return index > 0;
+		}
+
+		@Override
+		public MarsTableRowObject previous() {
+			return new MarsTableRowObject(index-1);
+		}
+
+		@Override
+		public int nextIndex() {
+			return index + 1;
+		}
+
+		@Override
+		public int previousIndex() {
+			return index - 1;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void set(MarsTableRowObject e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void add(MarsTableRowObject e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
 }
