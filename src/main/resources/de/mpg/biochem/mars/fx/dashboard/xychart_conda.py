025483f872864957a9c0f6b4ae35ec1309149339
Karl Duderstadt <duderstadt@biochem.mpg.de>
2022-04-23 21:30:30 +0200

    prevent overlapping x values in xychart_conda.py template

--- src/main/resources/de/mpg/biochem/mars/fx/dashboard/xychart_conda.py
+++ src/main/resources/de/mpg/biochem/mars/fx/dashboard/xychart_conda.py
@@ -6,6 +6,7 @@ import marspylib as mars
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 
 d = []
@@ -19,7 +20,10 @@ df = pd.DataFrame(d)
 
 sns.set_theme(style="white")
 ax = sns.pointplot(data=df, x="X", y="Y", hue="group", 
-			palette=sns.color_palette("husl", 3)).set(title="XY Chart")
+	palette=sns.color_palette("husl", 3))
+ax.xaxis.set_major_locator(ticker.MultipleLocator(5))
+ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+ax.set(title="XY Chart")
 
 fig = plt.gcf()
 fig.set_size_inches(width, height)
