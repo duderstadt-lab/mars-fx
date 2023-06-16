aa62e896e8806eefd8171668322a77f7079c2f9e
Karl Duderstadt <duderstadt@biochem.mpg.de>
2023-06-16 17:00:30 +0200

    BdvSourceOptionsPane: ensure video path is saved

--- src/main/java/de/mpg/biochem/mars/fx/molecule/metadataTab/BdvSourceOptionsPane.java
+++ src/main/java/de/mpg/biochem/mars/fx/molecule/metadataTab/BdvSourceOptionsPane.java
@@ -218,6 +218,7 @@ public class BdvSourceOptionsPane extends VBox {
 		HBox.setMargin(pathValidation, new Insets(0, 5, 10, 5));
 		pathBox.getChildren().add(pathValidation);
 
+		/*
 		pathField.textProperty().addListener((observable, oldValue, newValue) -> {
 			File file = new File(pathField.getText());
 			if (file.exists()) pathValidation.setGraphic(check);
@@ -225,7 +226,7 @@ public class BdvSourceOptionsPane extends VBox {
 
 			if (marsBdvSource != null) marsBdvSource.setPath(pathField.getText());
 		});
-
+*/
 		pathButton = new Button("Browse");
 		pathButton.setPrefWidth(70);
 		pathButton.setMaxWidth(70);
@@ -324,6 +325,7 @@ public class BdvSourceOptionsPane extends VBox {
 		pathField.textProperty().addListener((observable, oldValue, newValue) -> {
 			pathDetectionPause.setOnFinished(event -> {
 				if (marsBdvSource == null) return;
+				else marsBdvSource.setPath(pathField.getText());
 
 				boolean pathExists, datasetExists;
 				try {
