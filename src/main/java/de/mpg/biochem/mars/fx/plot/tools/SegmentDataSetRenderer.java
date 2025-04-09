2afde321aafadab38cb32485ae534af1aa285ba7
karlduderstadt <duderstadt@biochem.mpg.de>
2025-04-09 14:12:21 +0200

    WIP update SegmentDataSetRenderer to chartfx 11.3

--- src/main/java/de/mpg/biochem/mars/fx/plot/tools/SegmentDataSetRenderer.java
+++ src/main/java/de/mpg/biochem/mars/fx/plot/tools/SegmentDataSetRenderer.java
@@ -1,5 +1,36 @@
+/*-
+ * #%L
+ * JavaFX GUI for processing single-molecule TIRF and FMT data in the Structure and Dynamics of Molecular Machines research group.
+ * %%
+ * Copyright (C) 2018 - 2025 Karl Duderstadt
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+
 package de.mpg.biochem.mars.fx.plot.tools;
 
+import io.fair_acc.chartfx.renderer.spi.AbstractErrorDataSetRendererParameter;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.FillRule;
@@ -17,7 +48,6 @@ import io.fair_acc.chartfx.utils.FastDoubleArrayCache;
 import io.fair_acc.dataset.DataSet;
 import io.fair_acc.dataset.DataSetError.ErrorType;
 import io.fair_acc.dataset.utils.ProcessingProfiler;
-import io.fair_acc.chartfx.renderer.spi.AbstractErrorDataSetRendererParameter;
 
 /**
  * Renders data points with error bars and/or error surfaces It can be used e.g. to render horizontal and/or vertical
@@ -39,14 +69,14 @@ public class SegmentDataSetRenderer extends AbstractErrorDataSetRendererParamete
 	private final DataSetStyleParser styleParser = DataSetStyleParser.newInstance();
 
 	/**
-	 * Creates new <code>SegmentDataSetRenderer</code>.
+	 * Creates new <code>ErrorDataSetRenderer</code>.
 	 */
 	public SegmentDataSetRenderer() {
 		this(3);
 	}
 
 	/**
-	 * Creates new <code>SegmentDataSetRenderer</code>.
+	 * Creates new <code>ErrorDataSetRenderer</code>.
 	 *
 	 * @param dashSize initial size (top/bottom cap) on top of the error bars
 	 */
@@ -526,7 +556,7 @@ public class SegmentDataSetRenderer extends AbstractErrorDataSetRendererParamete
 	}
 
 	/**
-	 * @return the instance of this SegmentDataSetRenderer.
+	 * @return the instance of this ErrorDataSetRenderer.
 	 */
 	@Override
 	protected SegmentDataSetRenderer getThis() {
