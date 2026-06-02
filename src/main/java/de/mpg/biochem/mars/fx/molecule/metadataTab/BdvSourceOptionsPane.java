e1c8628be0ff06f2023550fb0494ece1a7967fe6
karlduderstadt <duderstadt@biochem.mpg.de>
2026-06-02 18:59:07 +0200

    license year updates

--- src/main/java/de/mpg/biochem/mars/fx/molecule/metadataTab/BdvSourceOptionsPane.java
+++ src/main/java/de/mpg/biochem/mars/fx/molecule/metadataTab/BdvSourceOptionsPane.java
@@ -2,17 +2,17 @@
  * #%L
  * JavaFX GUI for processing single-molecule TIRF and FMT data in the Structure and Dynamics of Molecular Machines research group.
  * %%
- * Copyright (C) 2018 - 2025 Karl Duderstadt
+ * Copyright (C) 2018 - 2026 Karl Duderstadt
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
